3c1c182cb081ccea9529875f36ba751cbdc27ef4
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-03-31 13:10:19 -0600

    Configuracion de la dependencia mdsvx

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,22 @@
 import adapter from '@sveltejs/adapter-auto';
 import { relative, sep } from 'node:path';
+import { mdsvex } from 'mdsvex';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md']
+		})
+	],
+
 	compilerOptions: {
-		// defaults to rune mode for the project, execept for `node_modules`. Can be removed in svelte 6.
+	
 		runes: ({ filename }) => {
 			const relativePath = relative(import.meta.dirname, filename);
 			const pathSegments = relativePath.toLowerCase().split(sep);
@@ -14,9 +26,6 @@ const config = {
 		}
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
