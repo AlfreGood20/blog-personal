e8f10ac92c7278d0dd680809f3a0c51fc597aa9b
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-04-02 16:20:20 -0600

    new: Componentes nuevos para fichero markdown

---
+++ src/components/Imagen.svelte
@@ -0,0 +1,15 @@
+
+<script>
+    let {imagen, nombre} = $props();
+</script>
+
+<img  src={imagen} 
+    class=
+    "active:-translate-y-6 
+    rounded-lg 
+    hover:-translate-y-2 
+    hover:shadow-2xl 
+    transition-all 
+    duration-200"
+     
+    alt={nombre}/>
---
+++ src/components/Information.svelte
@@ -0,0 +1,16 @@
+<script>
+    let {fecha, tiempoLectura} = $props();
+</script>
+
+<div class="flex flex-row text-xs gap-5 items-center justify-between">
+
+     <div class="flex items-center gap-1">
+        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+        <label for="tiempo">{tiempoLectura}</label>
+    </div>
+
+    <label for="fecha">Ultima actulización: {fecha}</label>
+
+</div>
