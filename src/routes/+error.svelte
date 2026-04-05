99ad88bec6b2b3a5a17e942fe563839d6735c6ed
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-04-05 14:59:03 -0600

    new: Manejo de errores

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from "$app/state";
+</script>
+
+
+<div class="flex pt-60 pb-42  md:pt-40 md:pb-25 items-center justify-center p-4">
+    <div class="flex flex-col items-center justify-center text-center">
+
+        <h1 class="text-6xl font-extrabold text-error mb-2">{page.status}</h1>
+        <p class="text-lg opacity-80 mb-6">{page.error.message}</p>
+        
+        <a class="btn btn-outline" href="/">Regresar</a>
+    </div>
+</div>
