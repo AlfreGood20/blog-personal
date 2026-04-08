a35e3a971cb3ac490121a09fcae66a630fbd01e2
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-04-08 14:49:30 -0600

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
     import Footer from "../components/Footer.svelte";
     import Navbar from "../components/Navbar.svelte";
-
+    import '../app.css';
+    
     let { children } = $props();
 </script>
 
